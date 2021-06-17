12d1f05fa08b69929d34796d39fb54766bba2159
Micheal Shokunbi <mightysho123@yahoo.com>
2021-06-17 12:47:49 -0700

    commit Hello , printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,16 +3,12 @@
 /**
  * main - Entry point
  *
- * printf - To print size of various types
+ * printf - to print "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
