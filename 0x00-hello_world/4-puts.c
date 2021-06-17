bc6bcc9e32eb0a1822a93e7e1f999394229dadab
Micheal Shokunbi <mightysho123@yahoo.com>
2021-06-17 12:52:50 -0700

    commit Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts - To write a new line or string to the output. It prints
+ * the passed string with a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
