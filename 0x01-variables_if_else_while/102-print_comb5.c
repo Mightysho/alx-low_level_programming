aac3101f296ebce3efc7e24c293cb685748872a2
Micheal Shokunbi <mightysho123@yahoo.com>
2021-06-18 13:48:35 -0700

    commit Software is eating the  World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,33 +3,33 @@
 /**
  * main - entry point
  *
- * putchar - two pairs of numbers that do not repeat
+ * putchar - to prints all possible combinations of two two-digit numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+int i, j;
 
-	for (j = 0; j <= 99; j++)
-	{
-		for (i = j; i <= 99; i++)
-		{
-			if (i != j)
-			{
-				putchar(j / 10 + 48);
-				putchar(j % 10 + 48);
-				putchar(' ');
-				putchar(i / 10 + 48);
-				putchar(i % 10 + 48);
-			}
-			if (j * 100 + i != 9899)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
