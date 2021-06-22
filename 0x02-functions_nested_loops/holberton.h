a4f206f2e25317bf8b04d1ae844de46ca0d4a46c
Micheal Shokunbi <mightysho123@yahoo.com>
2021-06-22 05:37:09 -0700

    commit holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+#endif
