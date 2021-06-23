0a9109378cf0a2483a657f740bb9700b760c9c4f
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-06-24 00:26:03 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,49 +7,48 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+int a, b, c, d, e, f;
 
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b;
-				d = c / 100;
-				e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
+if (n >= 0 && n < 15)
+{
+for (a = 0; a <=  n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = a * b;
+d = c / 100;
+e = (c /  10) % 10;
+f = (c % 100) % 10;
+if (b == 0)
+{
+_putchar('0');
+}
+else if (c < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + f);
+}
+else if (c < 100)
+{
+_putchar(' ');
+_putchar('0' + e);
+_putchar('0' + f);
+}
+else
+{
+_putchar('0' + d);
+_putchar('0' + e);
+_putchar('0' + f);
+}
+if (b < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
 _putchar('\n');
 }
 }
 }
 }
-
