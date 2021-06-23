2fd4e1aac505cc8cba45dd50d6c0ee44a70cabc7
Micheal Shokunbi <mightysho123@yahoo.com>
2021-06-23 16:38:31 -0700

    commit The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,47 @@
 #include "holberton.h"
+
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-int a, b, c, d, e, f;
+	int i, j, k;
 
-if (n >= 0 && n < 15)
-{
-for (a = 0; a <=  n; a++)
-{
-for (b = 0; b <= n; b++)
-{
-c = a * b;
-d = c / 100;
-e = (c /  10) % 10;
-f = (c % 100) % 10;
-if (b == 0)
-{
-_putchar('0');
-}
-else if (c < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + f);
-}
-else if (c < 100)
-{
-_putchar(' ');
-_putchar('0' + e);
-_putchar('0' + f);
-}
-else
-{
-_putchar('0' + d);
-_putchar('0' + e);
-_putchar('0' + f);
-}
-if (b < n)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-_putchar('\n');
-}
-}
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
