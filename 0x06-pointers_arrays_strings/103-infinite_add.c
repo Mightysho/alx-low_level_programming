492ed66ab989faa2d30bd3e5e2b490bc99f994ab
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-07-01 21:46:36 +0100

    To be edited later

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- *infinite_add - add two integer in buffe r.
- *@n1: char
- *@n2: char
- *@r: char
- *@size_r: integer
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
  *
- *Return: pointer to char
+ * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
