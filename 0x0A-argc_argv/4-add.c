19d10672ad81541b71031b18b2f9b38b98bfee5d
Micheal Shokunbi <mightysho123@yahoo.com>
2021-07-13 07:12:27 -0700

    commit 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,16 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-        int i, j, res = 0;
+int i, j, res = 0;
 
-        if (argc > 1)
-                for (i = 1; i < argc; i++)
-                {
-                        for (j = 0; argv[i][j]; j++)
-                                if (argv[i][j] < '0' || argv[i][j] > '9')
-                                        return (printf("Error\n"), 1);
-                        res += atoi(argv[i]);
-                }
-        printf("%i\n", res);
-        return (0);
+if (argc > 1)
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+if (argv[i][j] < '0' || argv[i][j] > '9')
+return (printf("Error\n"), 1);
+res += atoi(argv[i]);
+}
+printf("%i\n", res);
+return (0);
 }
