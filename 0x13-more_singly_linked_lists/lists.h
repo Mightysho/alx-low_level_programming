993a534860237c1dd40cb96786c628c3ce22810f
Micheal Shokunbi <mightysho4sure2014@gmail.com>
2021-08-04 22:50:41 +0100

    commit update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,8 +23,7 @@ struct listint_t *next;
 } listint_t;
 
 int _putchar(char c);
-// size_t print_listint(const listint_t *he);
-size_t print_listint_safe(const listint_t *head);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -36,7 +35,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-// size_t print_listint_safe(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif /* LISTS_H */
